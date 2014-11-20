eee3318398a4f137b96e61b23f77e56de085164e
Chupitov <meyeradriano@gmail.com>
2014-11-20 14:45:14 +0100

    fds

---
+++ src/edu/example/client/gui/MyPieChart.java
@@ -0,0 +1,91 @@
+package edu.example.client.gui;
+
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.visualization.client.AbstractDataTable;
+import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
+import com.google.gwt.visualization.client.DataTable;
+import com.google.gwt.visualization.client.Selection;
+import com.google.gwt.visualization.client.events.SelectHandler;
+import com.google.gwt.visualization.client.visualizations.PieChart;
+import com.google.gwt.visualization.client.visualizations.PieChart.Options;
+
+public class MyPieChart {
+
+	public PieChart getPieChart() {
+	
+		PieChart pie = new PieChart(createTable(), createOptions());
+
+		//pie.addSelectHandler(createSelectHandler(pie));		
+		return pie;
+	}
+
+	private SelectHandler createSelectHandler(final PieChart chart) {
+		return new SelectHandler() {
+			@Override
+			public void onSelect(SelectEvent event) {
+				String message = "";
+
+				// May be multiple selections.
+				JsArray<Selection> selections = chart.getSelections();
+
+				for (int i = 0; i < selections.length(); i++) {
+					// add a new line for each selection
+					message += i == 0 ? "" : "\n";
+
+					Selection selection = selections.get(i);
+
+					if (selection.isCell()) {
+						// isCell() returns true if a cell has been selected.
+
+						// getRow() returns the row number of the selected cell.
+						int row = selection.getRow();
+						// getColumn() returns the column number of the selected
+						// cell.
+						int column = selection.getColumn();
+						message += "cell " + row + ":" + column + " selected";
+					} else if (selection.isRow()) {
+						// isRow() returns true if an entire row has been
+						// selected.
+
+						// getRow() returns the row number of the selected row.
+						int row = selection.getRow();
+						message += "row " + row + " selected";
+					} else {
+						// unreachable
+						message += "Pie chart selections should be either row selections or cell selections.";
+						message += "  Other visualizations support column selections as well.";
+					}
+				}
+
+				Window.alert(message);
+			}
+		};
+	}
+
+	private AbstractDataTable createTable() {
+		DataTable data = DataTable.create();
+		data.addColumn(ColumnType.STRING, "Task");
+		data.addColumn(ColumnType.NUMBER, "Hours per Day");
+		data.addRows(4);
+		data.setValue(0, 0, "Spain");
+		data.setValue(0, 1, 14);
+		data.setValue(1, 0, "Italy");
+		data.setValue(1, 1, 10);
+		data.setValue(2, 0, "Honduras");
+		data.setValue(2, 1, 3);
+		data.setValue(3, 0, "Indonesia");
+		data.setValue(3, 1, 11);
+		return data;
+	}
+
+	private Options createOptions() {
+		Options options = Options.create();
+		options.setWidth(800);
+		options.setHeight(480);
+		options.set3D(true);
+		options.setTitle("Import");
+		return options;
+	}
+
+}
